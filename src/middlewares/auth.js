69e27bfc2e49f1e11a445aedebe1c07f55114e84
Mane <storagebox7396@gmail.com>
2025-01-28 23:47:44 +0530

    token validation

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth=async (req,res,next)=>{
  try{ 
     const {token}=req.cookies
   if(!token){
-    throw new Error("token is not valid")
+   res.status(401).send("please login")
   }
   const decodeObj=await jwt.verify(token,"mane")
   const {_id}=decodeObj
