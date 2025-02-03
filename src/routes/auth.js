6d09c2875eb68d4411b53170fcc5f4f22de8044e
Mane <storagebox7396@gmail.com>
2025-02-03 22:00:11 +0530

    changes to support frontend

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -19,8 +19,12 @@ authRouter.post("/signup", async (req, res) => {
             password: passwordHash
         });
 
-        await user.save();
-        res.send("User added successfully");
+        const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+    res.json({ message: "User Added successfully!", data: savedUser });
     } catch (err) {
         console.error("Error:", err.message);
         res.status(400).send("Error saving the user: " + err.message);
